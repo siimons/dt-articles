9d8a131ca39f7c1f1c477b8513ffcbbdaf02ae31
Vlad <vlad.kanatik@ya.ru>
2024-09-08 17:13:59 +0300

    edit routes and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,23 @@
+# Используем минимальный образ Python
 FROM python:3.11-alpine
 
+# Устанавливаем зависимости для работы FastAPI и Uvicorn
+RUN apk add --no-cache gcc musl-dev libffi-dev
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Копируем файл зависимостей
+COPY requirements/base.txt /app/requirements.txt
+
+# Устанавливаем зависимости
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+# Копируем все файлы приложения
+COPY . .
+
+# Открываем порт для приложения
+EXPOSE 8000
+
+# Запускаем FastAPI приложение с Uvicorn
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- src/router.py
+++ src/router.py
@@ -18,35 +18,35 @@ from src.service import (
 
 router = APIRouter()
 
-@router.post('/article/')
+@router.post('/api/article/')
 def create_article(article: ArticleCreate):
 
     result = add_article_with_tags(article.title, article.content, article.tags, article.updated_at)
     
     return result
 
-@router.put('/article/{id}')
+@router.put('/api/article/{id}')
 def update_article(article: ArticleUpdate):
     
     result = update_article_and_tags(article.id, article.title, article.contents, article.tags, article.updated_at)
     
     return result
 
-@router.delete('/article/{id}')
+@router.delete('/api/article/{id}')
 def delete_article(article: ArticleDelete):
     
     result = delete_article_from_db(article.id)
     
     return result
 
-@router.get('/articles/ids', response_model=ArticleIDs)
+@router.get('/api/articles/ids', response_model=ArticleIDs)
 def fetch_article_ids():
     
     article_ids = get_all_article_ids()
     
     return ArticleIDs(ids=article_ids)
 
-@router.get('/article/{article_id}', response_model=Article)
+@router.get('/api/article/{article_id}', response_model=Article)
 def read_article(article_id: int):
     
     result = get_article_by_id(article_id)
