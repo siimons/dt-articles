f4a187450aa0e8fe8ca385191688dbbf856fd83a
Simon <siimonchernyk@gmail.com>
2025-01-18 21:13:37 +0300

    Update CRUD

--- app/api/v1/crud.py
+++ app/api/v1/crud.py
@@ -2,18 +2,27 @@ from app.core.database import Database
 from app.api.v1.schemas import ArticleCreate
 from app.api.v1.exceptions import ArticleAlreadyExistsException
 
-db = Database()
+from app.core.logging import logger
 
-async def create_article(article: ArticleCreate) -> dict:
-    query = """
-    INSERT INTO articles (title, content, tags) 
-    VALUES (%s, %s, %s) RETURNING id, title, content, tags, created_at;
+async def create_article(db: Database, article: ArticleCreate) -> dict:
     """
-    existing_article = await db.fetch("SELECT * FROM articles WHERE title = %s;", article.title)
+    Создание новой статьи.
+    """
+    query_check = "SELECT id FROM articles WHERE title = %s"
+    existing_article = await db.fetch(query_check, article.title)
+    
     if existing_article:
+        logger.error(f"Статья с заголовком '{article.title}' уже существует.")
         raise ArticleAlreadyExistsException(article.title)
 
-    result = await db.fetch(query, article.title, article.content, article.tags)
-    if not result:
-        return None
-    return result[0]
+    query_create = """
+    INSERT INTO articles (title, content, tags)
+    VALUES (%s, %s, %s)
+    """
+    try:
+        new_article = await db.fetch(query_create, article.title, article.content, article.tags)
+        logger.success(f"Статья '{article.title}' успешно создана.")
+        return new_article[0]
+    except Exception as e:
+        logger.error(f"Ошибка при создании статьи '{article.title}': {e}")
+        raise
